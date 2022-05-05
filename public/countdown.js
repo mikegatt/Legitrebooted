513e944aee7a3b12aa3c3f2c88ad32f2d5c0b070
Gejakiat <georgejkatkinson@gmail.com>
2022-05-05 18:44:09 +0100

    commit

--- public/countdown.js
+++ public/countdown.js
@@ -1,4 +1,4 @@
-var countDownDate = new Date("May 06, 2022 00:00:00").getTime();
+var countDownDate = new Date("May 06, 2022 12:00:00").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
